1cda7d06ad2d4914f6fd2c680c439dc784ebeff6
fredalex6 <frederik.a.adlandsvik@hotmail.com>
2026-01-28 11:55:46 +0100

    Lager NN

--- src/project/model.py
+++ src/project/model.py
@@ -84,8 +84,24 @@ def forward(
     # Oppgave 4.1: Start
     #######################################################################
 
-    # Placeholder initialization — replace this with your implementation
-    out = None
+    # Definerer aktiveringsfunksjonen
+    def sigma(z):
+        return jnp.tanh(z)
+    
+
+    x_norm = (x - cfg.x_min) / (cfg.x_max - cfg.x_min)
+    y_norm = (y - cfg.y_min) / (cfg.y_max - cfg.y_min)
+    t_norm = (t - cfg.t_min) / (cfg.t_max - cfg.t_min)
+    
+    a = jnp.stack([x_norm, y_norm, t_norm], axis=-1)
+
+
+    for W, b in nn_params[:-1]:
+        a = sigma(a @ W + b)
+
+    w_out, b_out = nn_params[-1]
+   
+    out = (a @ w_out + b_out).squeeze()
 
     #######################################################################
     # Oppgave 4.1: Slutt
