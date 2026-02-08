d0783f9acf1c59bc4e37457aa268a4d5398923dd
fredalex6 <frederik.a.adlandsvik@hotmail.com>
2026-02-08 12:37:44 +0100

    håndter fil-importering i ulike oppgaver

--- src/project/__init__.py
+++ src/project/__init__.py
@@ -7,6 +7,21 @@ from .optim import adam_step, init_adam
 from .sampling import sample_bc, sample_ic, sample_interior
 from .train import train_nn, train_pinn
 
+# Import for Adaptive Loss Weighting
+#from .train_woptim import train_nn, train_pinn
+
+
+
+# These ones should be imported when using
+"""from .config import Config, load_config
+from .data_am import generate_training_data
+from .fdm_am import solve_heat_equation
+from .loss_am import data_loss, ic_loss, physics_loss
+from .model_am import forward, init_nn_params, init_pinn_params, predict_grid
+from .optim import adam_step, init_adam
+from .sampling_am import sample_bc, sample_ic, sample_interior
+from .train_am import train_nn, train_pinn"""
+
 __all__ = [
     "Config",
     "load_config",
